3717fb449fd46d5dac9409ba92f955d87053b214
satyasaibathina <satyasaibathina@gmail.com>
2026-07-21 12:41:27 +0530

    fix: update hardcoded file paths for new laptop (Sai Teja Project)

--- python/upload_files.py
+++ python/upload_files.py
@@ -3,7 +3,7 @@ import os
 from connect_snowflake import get_connection
 
 # Change this to your project path
-DATA_FOLDER = r"D:\Projects\Enterprise_Logistics_Data_Platform\data"
+DATA_FOLDER = r"D:\Sai Teja Project\Enterprise_Logistics_Data_Platform\data"
 
 conn = get_connection()
 cur = conn.cursor()
